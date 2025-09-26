d5499ca2f376174bc3d7599c04a9dbb36f1291da
khanh-gg <nguyenletrongkhanh0604@gmail.com>
2025-09-26 08:36:52 +0700

    Thêm nút Tạm biệt

--- git/src/App.js
+++ git/src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       {/* Nút Xin chào */}
       <button onClick={() => alert("xin chào")}>Xin chào</button>
+
+      <br /><br />
+
+      {/* Nút Tạm biệt */}
+      <button onClick={() => alert("tạm biệt")}>Tạm biệt</button>
     </div>
   );
 }
